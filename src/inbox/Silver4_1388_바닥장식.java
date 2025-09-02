b9c669d1db0ac1fcbdd5376952997d439d6482fe
infreeJ <kasious97@naver.com>
2025-09-02 23:08:27 +0900

    문자열이 문제인듯

--- src/inbox/Silver4_1388_바닥장식.java
+++ src/inbox/Silver4_1388_바닥장식.java
@@ -27,28 +27,54 @@ public class Silver4_1388_바닥장식 {
         // 조합한 바닥은 빈 문자열로 바꿔서 계산 -> null이 아니면서 |이거나 -인 경우
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
+
+
                 if(deco[i][j].equals("-")) {
-                    System.out.println("count 증가 -> deco[" + i + "][" + j + "]");
+                    System.out.println("count = " + count + " -> deco[" + i + "][" + j + "]");
                     count++;
-                    for(int k = 0; k < col -j; k++) {
-                        if(deco[i][j+k].equals("-")) {
-                            deco[i][j+k] = "";
-                        } else if (deco[i][j+k].equals("|")) {
+                    for(int k = j; k < col; k++) {
+                        if(deco[i][k].equals("-")) {
+                            deco[i][k] = "*";
+                        } else if (deco[i][k].equals("|")) {
                             break;
                         }
                     }
-                    //deco[j][i] = "";
+
+
+                    // 테스트 for문 ------
+                    for(int a = 0; a < row; a++) {
+                        for (int b = 0; b < col; b++) {
+                            System.out.print(deco[a][b]);
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    // ------------------
+
+
                 } else if (deco[i][j].equals("|")) {
-                    System.out.println("count 증가 -> deco[" + i + "][" + j + "]");
+                    System.out.println("count = " + count + " -> deco[" + i + "][" + j + "]");
                     count++;
-                    for(int k = 0; k < row-i; k++) {
-                        if(deco[i+k][j].equals("|")) {
-                            deco[i+k][j] = "";
-                        } else if (deco[i+k][j].equals("-")) {
+                    for(int k = i; k < row; k++) {
+                        if(deco[k][j].equals("|")) {
+                            deco[k][j] = "*";
+                        } else if (deco[k][j].equals("-")) {
                             break;
                         }
                     }
-                    //deco[j][i] = "";
+
+
+                    // 테스트 for문 ------
+                    for(int a = 0; a < row; a++) {
+                        for (int b = 0; b < col; b++) {
+                            System.out.print(deco[a][b]);
+                        }
+                        System.out.println();
+                    }
+                    System.out.println();
+                    // ------------------
+
+
                 }
             }
         }
