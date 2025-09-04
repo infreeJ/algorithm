980671d3a50d5e239542b089ed022633e2206101
infreeJ <kasious97@naver.com>
2025-09-04 20:20:25 +0900

    Bronze2_3062_수뒤집기

---
+++ src/inbox/Bronze2_3062_수뒤집기.java
@@ -0,0 +1,54 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze2_3062_수뒤집기 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int testCase = scan.nextInt();
+        scan.nextLine();
+
+        for(int i=0; i<testCase; i++) {
+            int num = scan.nextInt();
+            scan.nextLine();
+            int numClone = num;
+
+            int reversed = 0;
+            while (numClone != 0) {
+                int digit = numClone % 10;
+                reversed = reversed * 10 + digit;
+                numClone /= 10;
+            }
+
+            int sum = num + reversed;
+            System.out.println("num = " + num);
+            System.out.println("reversed = " + reversed);
+            System.out.println("sum = " + sum);
+
+            String[] sumArr = String.valueOf(sum).split("");
+
+            int k = sumArr.length-1;
+            System.out.println("시작 전 k는 " + k);
+            boolean Yes = true;
+            for(int j = 0; j <sumArr.length/2; j++) {
+                System.out.println("현재 j는 " + j + "현재 k는 " + k + "  " + sumArr[j] + " / " + sumArr[k]);
+                if(!sumArr[j].equals(sumArr[k])) {
+                    Yes = false;
+                    break;
+                }
+                k--;
+            }
+
+
+            if(Yes) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+
+        }
+        //System.out.println(sb.toString());
+    }
+}
