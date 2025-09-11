1c4ddfbb093c0f04664e71d3569507bc5f478298
infreeJ <kasious97@naver.com>
2025-09-11 15:53:04 +0900

    Bronze1_1259_팰린드롬수

---
+++ src/inbox/Bronze1_1259_팰린드롬수.java
@@ -0,0 +1,27 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze1_1259_팰린드롬수 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        String input = scan.nextLine();
+
+        if(!input.equals("0")) {
+            while(!input.equals("0")) {
+                sb.append(input);
+                sb.reverse();
+
+                if(input.contentEquals(sb.toString())) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+                sb.delete(0, sb.length());
+                input = scan.nextLine();
+            }
+        }
+    }
+}
