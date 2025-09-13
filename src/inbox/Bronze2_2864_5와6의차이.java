15c0baa34fdc8e793c7762bf2a1bb1474ef4968d
infreeJ <kasious97@naver.com>
2025-09-13 22:14:38 +0900

    Bronze2_2864_5와6의차이

---
+++ src/inbox/Bronze2_2864_5와6의차이.java
@@ -0,0 +1,18 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze2_2864_5와6의차이 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        String[] input = scan.nextLine().split(" ");
+
+
+        int min = Integer.parseInt(input[0].replace("6", "5")) + Integer.parseInt(input[1].replace("6", "5"));
+        int max = Integer.parseInt(input[0].replace("5", "6")) + Integer.parseInt(input[1].replace("5", "6"));
+
+        System.out.println(min + " " + max);
+    }
+}
