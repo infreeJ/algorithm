9a21c8f8347abaee7fbd6bac7f12cb0d6ad9a25f
infreeJ <kasious97@naver.com>
2025-09-14 16:22:44 +0900

    Bronze1_14659_한조서열정리하고옴ㅋㅋ

---
+++ src/inbox/Bronze1_14659_한조서열정리하고옴ㅋㅋ.java
@@ -0,0 +1,37 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze1_14659_한조서열정리하고옴ㅋㅋ {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+        scan.nextLine();
+
+        int[] mountains = new int[t];
+        for (int i = 0; i < t; i++) {
+            mountains[i] = scan.nextInt();
+        }
+
+
+        int kill = 0;
+        int tmp = 0;
+
+        for (int i = 0; i < mountains.length-1; i++) {
+            for (int j = i+1; j < mountains.length; j++) {
+                if(mountains[i] > mountains[j]) {
+                    tmp++;
+                    if(kill < tmp) {
+                        kill = tmp;
+                    }
+                } else {
+                    break;
+                }
+            }
+            tmp = 0;
+        }
+
+        System.out.println(kill);
+    }
+}
