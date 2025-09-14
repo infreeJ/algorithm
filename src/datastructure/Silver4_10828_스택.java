0e31364f4c984ce2dd7255721ec6496e6f6018ab
infreeJ <kasious97@naver.com>
2025-09-14 22:45:58 +0900

    Silver4_10828_스택

---
+++ src/datastructure/Silver4_10828_스택.java
@@ -0,0 +1,94 @@
+package datastructure;
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    private Node head;
+    private int count;
+
+    public Stack() {
+        head = null;
+        count = 0;
+    }
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        count++;
+    }
+
+    public int pop() {
+        if(count == 0) {
+            return -1;
+        } else {
+            int popData = head.data;
+            head = head.next;
+            count--;
+            return popData;
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public int empty() {
+        if(count == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int top() {
+        if(count == 0) {
+            return -1;
+        } else {
+            return head.data;
+        }
+    }
+
+}
+
+public class Silver4_10828_스택 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        scan.nextLine();
+
+        Stack stack = new Stack();
+
+        for (int i = 0; i < t; i++) {
+            String input = scan.nextLine();
+            if(input.contains("push")) {
+                String[] tmp = input.split(" ");
+                int data = Integer.parseInt(tmp[1]);
+                stack.push(data);
+            }
+            if(input.equals("pop")) {
+                System.out.println(stack.pop());
+            }
+            if(input.equals("size")) {
+                System.out.println(stack.size());
+            }
+            if(input.equals("empty")) {
+                System.out.println(stack.empty());
+            }
+            if(input.equals("top")) {
+                System.out.println(stack.top());
+            }
+        }
+
+    }
+}
