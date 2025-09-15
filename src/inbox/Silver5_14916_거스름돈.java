bd7327c642ea1d2e557a151a3f79e4ec3174e433
infreeJ <kasious97@naver.com>
2025-09-15 11:34:16 +0900

    Silver5_14916_거스름돈

---
+++ src/inbox/Silver5_14916_거스름돈.java
@@ -0,0 +1,41 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Silver5_14916_거스름돈 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int money = scan.nextInt();
+        scan.nextLine();
+
+        int five = 0;
+        int two = 0;
+
+        five = money / 5;
+        money %= 5;
+        if(money % 2 == 0) {
+            two = money / 2;
+            money -= 2 * two;
+        } else {
+            if(five != 0) {
+                five--;
+                money += 5;
+            }
+            two = money / 2;
+            money -= 2 * two;
+        }
+
+        boolean success = false;
+
+        if(money == 0) {
+            success = true;
+        }
+
+        if(success) {
+            System.out.println(five + two);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
