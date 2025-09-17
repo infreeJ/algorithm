7ab12a0629058a53fd5522696c27d66e647f0f6b
infreeJ <kasious97@naver.com>
2025-09-17 13:16:28 +0900

    Bronze3_10103_주사위게임

---
+++ src/inbox/Bronze3_10103_주사위게임.java
@@ -0,0 +1,31 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze3_10103_주사위게임 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+        scan.nextLine();
+
+
+        int chang = 100;
+        int sang = 100;
+
+
+        for (int i = 0; i < t; i++) {
+            String[] input = scan.nextLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            if(a > b) {
+                sang -= a;
+            } else if (a < b) {
+                chang -= b;
+            }
+        }
+
+        System.out.println(chang);
+        System.out.println(sang);
+    }
+}
