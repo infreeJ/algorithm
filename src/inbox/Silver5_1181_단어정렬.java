a8de4f67b2bbbbecf463ab0baf02326b03a6dc46
infreeJ <kasious97@naver.com>
2025-09-18 12:18:44 +0900

    Silver5_1181_단어정렬

---
+++ src/inbox/Silver5_1181_단어정렬.java
@@ -0,0 +1,45 @@
+package inbox;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Silver5_1181_단어정렬 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+
+            int t = Integer.parseInt(br.readLine());
+            List<String> list = new ArrayList<>();
+
+            for (int i = 0; i < t; i++) {
+                list.add(br.readLine());
+            }
+
+            list.sort(Comparator.comparingInt(String::length).thenComparing(Comparator.naturalOrder()));
+
+
+            String tmp = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
+                if(tmp.equals(list.get(i))) {
+                    list.remove(i);
+                    i--;
+                } else {
+                    tmp = list.get(i);
+                }
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
