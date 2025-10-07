9a3e64c0f2255829499a548dd171b6c8c67e6eac
infreeJ <kasious97@naver.com>
2025-10-07 22:14:08 +0900

    Bronze2_3052_나머지

---
+++ src/inbox/Bronze2_3052_나머지.java
@@ -0,0 +1,26 @@
+package inbox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze2_3052_나머지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int[] arr = new int[42];
+        for (int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42]++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
