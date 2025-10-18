d8043d6ac434b64c4df71fedf60a8c2f4486f64a
infreeJ <kasious97@naver.com>
2025-10-18 10:26:58 +0900

    Silver5_11004_K번째수

---
+++ src/Silver5_11004_K번째수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Silver5_11004_K번째수 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input1 = br.readLine().split(" ");
+        int N = Integer.parseInt(input1[0]);
+        int K = Integer.parseInt(input1[1]);
+
+        String[] input2 = br.readLine().split(" ");
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(input2[i]));
+        }
+
+        Collections.sort(list, Comparator.naturalOrder());
+
+        System.out.println(list.get(K-1));
+    }
+}
