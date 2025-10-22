9afd321ab75fe03054d5c0b7fdf7f52f46ce2fc2
infreeJ <kasious97@naver.com>
2025-10-22 10:54:39 +0900

    Silver4_9012_괄호

---
+++ src/inbox/Silver4_9012_괄호.java
@@ -0,0 +1,55 @@
+package inbox;
+
+import java.io.*;
+
+public class Silver4_9012_괄호 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int[] arr = new int[2];
+            String[] input = br.readLine().split("");
+            boolean isSuccess = true;
+
+            for (int j = 0; j < input.length; j++) {
+
+                boolean shouldBreak = false;
+
+                switch (input[j]) {
+                    case "(" :
+                        if(j == input.length-1 && arr[1] >= 1) {
+                            isSuccess = false;
+                            shouldBreak = true;
+                        }
+                        arr[0]++;
+                        break;
+                    case  ")" :
+                        if(j == 0 || arr[0] <= arr[1]) {
+                            isSuccess = false;
+                            shouldBreak = true;
+                        }
+                        arr[1]++;
+                        break;
+                }
+                if(shouldBreak) {
+                    break;
+                }
+            }
+
+            if(arr[0] == arr[1] && isSuccess) {
+                bw.write("YES");
+                bw.newLine();
+            } else {
+                bw.write("NO");
+                bw.newLine();
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
