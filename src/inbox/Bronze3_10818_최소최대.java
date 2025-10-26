8a43b994e5b644936e843066755965ef43eca3f8
infreeJ <kasious97@naver.com>
2025-10-26 23:07:03 +0900

    Bronze3_10818_최소최대

---
+++ src/inbox/Bronze3_10818_최소최대.java
@@ -0,0 +1,26 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze3_10818_최소최대 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        scan.nextLine();
+
+        String[] input = scan.nextLine().split(" ");
+
+        int min = 1000000;
+        int max = -1000000;
+        for (int i = 0; i < t; i++) {
+            if(min > Integer.parseInt(input[i])) {
+                min = Integer.parseInt(input[i]);
+            }
+
+            if(max < Integer.parseInt(input[i])) {
+                max = Integer.parseInt(input[i]);
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
