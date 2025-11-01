963d91ff62448bf69d1950817eee567c92b95f2c
infreeJ <kasious97@naver.com>
2025-11-01 23:33:32 +0900

    Bronze4_11720_숫자의합

---
+++ src/inbox/Bronze4_11720_숫자의합.java
@@ -0,0 +1,21 @@
+package inbox;
+
+import java.util.Scanner;
+
+public class Bronze4_11720_숫자의합 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+        scan.nextLine();
+
+        String[] input = scan.nextLine().split("");
+
+        int sum = 0;
+        for (int i = 0; i < input.length; i++) {
+            sum += Integer.parseInt(input[i]);
+        }
+
+        System.out.println(sum);
+    }
+}
