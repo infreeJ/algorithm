854873cecb2dcc6254df977826ed99b4a4576ae0
infreeJ <kasious97@naver.com>
2025-12-26 17:55:18 +0900

    Silver3_2606_바이러스

--- src/inbox/Silver3_2606_바이러스.java
+++ src/inbox/Silver3_2606_바이러스.java
@@ -3,54 +3,100 @@ package inbox;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Silver3_2606_바이러스 {
 
-    static boolean[] isVirus; // 각 컴퓨터 별 바이러스 유무
+    /*
+    접근 방법
+    1. HashSet<Computer>, 감염 여부 상태를 가진 computer 클래스를 정의한다.
+    2. 받은 입력으로 컴퓨터 간 연결 관계를 만든다. Computer.neighbors
+    3. 1번 컴퓨터부터 시작하여 연결된 컴퓨터를 재귀호출하며 감염시킨다.
+     */
+    
+    /*
+    책임 분배
+    1. 입력을 받고 결과를 출력하는 main 메서드
+    2. 상태 정보를 가지는 computer 클래스
+    3. 입력 받은 네트워크로 컴퓨터를 연결하는 addNeighbors 메서드
+    4. 감염되었는지 검증하고 감염시키는 infection 메서드
+     */
+
+    // 새로 감염되는 컴퓨터의 수
+    static int count = 0;
+
+
+    static class Computer {
+
+        Set<Computer> neighbors; // 연결된 컴퓨터들 (중복을 허용하지 않는 HashSet으로 객체 생성)
+        boolean isInfected = false; // 감염 여부
+
+        Computer(boolean isInfected) {
+            this.neighbors = new HashSet<>();
+            this.isInfected = isInfected;
+        }
+    }
+
+    /**
+     * 이미 연결되어 있는지 검증하고 연결되어 있지않다면 연결하는 메서드
+     */
+    static void addNeighbors(Computer computerA, Computer computerB) {
+        // 중복 추가를 방지하기 위해 이미 추가되어 있다면 return
+        if(!computerA.neighbors.contains(computerB)) {
+            computerA.neighbors.add(computerB);
+            computerB.neighbors.add(computerA);
+        }
+    }
+
+    
+    /**
+     * 감염 메서드
+     */
+    static void infection(Computer computer) {
+
+        // 현재 컴퓨터가 건강한 상태면 감염 및 count 증가
+        if(!computer.isInfected) {
+            computer.isInfected = true;
+            count++;
+        }
+
+        // 연결된 컴퓨터를 순환하며 infection() 재귀호출
+        computer.neighbors.forEach((nextComputer) -> {
+            
+            // 다음 컴퓨터가 감염된 상태일 경우 이미 방문했으므로 스킵한다.
+            if(!nextComputer.isInfected) {
+                infection(nextComputer);
+            }
+        });
+    }
+
+
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int computers = Integer.parseInt(br.readLine()); // 컴퓨터의 수
-        int t = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 쌍의 수
+        int computerNum = Integer.parseInt(br.readLine());
+        int networkNum = Integer.parseInt(br.readLine());
 
-        isVirus = new boolean[computers];
+        Computer[] computers = new Computer[computerNum];
+        for (int i = 0; i < computerNum; i++) {
+            computers[i] = new Computer(false);
+        }
+        computers[0].isInfected = true; // 1번 컴퓨터는 감염 상태
 
-        isVirus[0] = true; // 바이러스는 1번 컴퓨터에서 시작
-
-        int[][] pairs = new int[t][2];
-        for (int i = 0; i < t; i++) {
+        for (int i = 0; i < networkNum; i++) {
             String[] input = br.readLine().split(" ");
-            pairs[i][0] = Integer.parseInt(input[0]);
-            pairs[i][1] = Integer.parseInt(input[1]);
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            addNeighbors(computers[a-1], computers[b-1]);
         }
 
-        // 늦게 입력되어도 정상적으로 바이러스에 걸릴 수 있게 정렬
-        Arrays.sort(pairs, (int[] a, int[] b) -> {
-            return a[0] - b[0];
-        });
-
-        for (int i = 0; i < t; i++) {
-            virusCheck(pairs[i][0], pairs[i][1]);
-        }
-
-        int count = -1; // 1번 컴퓨터는 제외
-        for (int i = 0; i < isVirus.length; i++) {
-            if(isVirus[i]) {
-                count++;
-            }
-        }
+        // 루트 컴퓨터부터 infection() 호출
+        infection(computers[0]);
 
         System.out.println(count);
     }
-
-    // 연결된 한 쌍의 컴퓨터를 매개변수로 받아 바이러스를 전염시키는 메서드
-    public static void virusCheck(int current, int next) {
-        if(isVirus[current-1]) {
-            isVirus[next-1] = true;
-        }
-    }
-
 }
-
